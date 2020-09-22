288ed33b92832d8cc63aa0c6d6e129cd85218d64
BereniceTrejo <bere_t_v_44_@live.com.mx>
2020-09-22 18:54:17 -0500

    App moviles primer proyecto

---
+++ app/src/main/java/org/unitec/proyectounitec/MainActivity.kt
@@ -0,0 +1,13 @@
+package org.unitec.proyectounitec
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//El punto de partida de toda app es la MainActivity(Presenter)
+class MainActivity : AppCompatActivity() {
+    //fun en kotlin es una palabra reservada para implementar o crear una funcion o metodo
+    override fun onCreate(nombre: Bundle?) {
+        super.onCreate(nombre)
+        setContentView(R.layout.activity_main)
+    }
+}
