cd3025f14736f7b86b539a0451eb8f30786ad15a
BereniceTrejo <bere_t_v_44_@live.com.mx>
2020-09-22 19:52:16 -0500

    Se termino la clase de 22 sep 2020

--- app/src/main/java/org/unitec/proyectounitec/MainActivity.kt
+++ app/src/main/java/org/unitec/proyectounitec/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     //fun en kotlin es una palabra reservada para implementar o crear una funcion o metodo
     override fun onCreate(nombre: Bundle?) {
         super.onCreate(nombre)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.ejemplito)
     }
 }
